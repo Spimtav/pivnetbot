d4e971e631860ab644fbfa5e1608217f058cd6b1
Claire Tinati <ctinati@pivotal.io>
2018-12-14 17:20:55 -0500

    Cleaned prints out of middleware file
    Signed-off-by: Claire Tinati <ctinati@pivotal.io>

--- authorization_middleware.rb
+++ authorization_middleware.rb
@@ -8,16 +8,12 @@ class SlackAuthorizer
 
   def call(env)
     req = Rack::Request.new(env)
-    puts "FOR DEBUGGING, REQ IS: #{req.body.read}"
-    req.body.rewind
     params = JSON.parse(req.body.read)
-    puts "Parsed params are: #{params}"
     req.body.rewind
 
     if params['token'] && params['token'] == ENV['PIVNETBOT_VERIFICATION_TOKEN']
       @app.call(env)
     else
-      puts 'Middleware denied a message because token not present or malformed'
       UNAUTHORIZED_RESPONSE
     end
   end
